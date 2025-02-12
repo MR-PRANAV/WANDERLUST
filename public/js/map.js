068f455ae464a567ccefde9644881eaae611c3df
MR-PRANAV <pranavpatil020389@gmail.com>
2025-02-12 21:01:55 +0530

    RE-COMMITING AFTER MAP RESOLVING

--- public/js/map.js
+++ public/js/map.js
@@ -1,10 +1,15 @@
-mapboxgl.accessToken = MAPTOKEN;
+mapboxgl.accessToken = MAPTOKEN_A;
 
   const map = new mapboxgl.Map({
       container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12',
       center: listing.geometry.coordinates,
+      // center: [-74.5, 40],
       zoom: 9 // starting zoom
   });
+
+
+  
 
   const marker1 = new mapboxgl.Marker({ color: 'red' })
         .setLngLat(listing.geometry.coordinates)
